d0c277a77f993676c2521113772fce59a68da7d3
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-02-21 00:50:34 +0530

    Create 1.Two Sum.cpp

---
+++ Leetcode/ARRAYS/1.Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        
+        
+        for(int i=0;i<arr.size();i++){
+            for(int j=i+1;j<arr.size();j++){
+                if(arr[i]+arr[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+        
+    }
+};
