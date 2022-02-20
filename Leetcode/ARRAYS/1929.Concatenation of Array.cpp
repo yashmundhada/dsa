4d35d6a62af7d71ea66c263de83a4424b308b75e
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-02-21 00:07:10 +0530

    Update and rename Leetcode/1929. Concatenation of Array to Leetcode/ARRAYS/1929.Concatenation of Array.cpp

--- Leetcode/ARRAYS/1929.Concatenation of Array.cpp
+++ Leetcode/ARRAYS/1929.Concatenation of Array.cpp
@@ -1,7 +1,4 @@
 
-
-
-
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
@@ -22,5 +19,22 @@ public:
         
         
         
+    }
+};
+
+
+
+APPROCH 2 without using extra space
+
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
     }
 };
