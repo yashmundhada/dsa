d52a7b0e9b762b752dfcd1f3d91389bf3b30d2c9
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-02-21 14:16:15 +0530

    Update 1480.Running Sum of 1d Array.cpp

--- Leetcode/ARRAYS/1480.Running Sum of 1d Array.cpp
+++ Leetcode/ARRAYS/1480.Running Sum of 1d Array.cpp
@@ -1,7 +1,5 @@
 https://leetcode.com/problems/running-sum-of-1d-array/
 
-
-
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
@@ -20,5 +18,18 @@ public:
         
         return ans;
         
+    }
+};
+
+
+APPROCH 2 - 
+    
+    
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& a) {
+        for( int i=1 ; i<a.size() ; i++ )
+            a[i] += a[i-1] ;
+        return a ;
     }
 };
