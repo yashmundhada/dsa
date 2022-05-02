78af237e58ec6a50a9b8b083a6fdd08f54099bdf
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-05-02 12:19:31 +0530

    Create Kadane's algo.cpp

---
+++ dsa450/Kadane's algo.cpp
@@ -0,0 +1,32 @@
+https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+
+
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long max_so_far = INT_MIN;
+        long long max_end_here = 0;
+        
+        for(int i = 0; i<n ; i++){
+            
+            //include current element
+            max_end_here = max_end_here + arr[i];
+            
+            //update the ans;
+            max_so_far = max(max_so_far,max_end_here);
+            
+            //if max_end_here is 0 then dont include
+            if(max_end_here<0){
+                max_end_here = 0;
+            }
+        }
+        return max_so_far;
+        
+    }
+};
