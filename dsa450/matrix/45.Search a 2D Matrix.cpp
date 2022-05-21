80f3dae1f251aebf28c97947b454a3b722673948
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-05-21 19:46:39 +0530

    Update 45.Search a 2D Matrix.cpp

--- dsa450/matrix/45.Search a 2D Matrix.cpp
+++ dsa450/matrix/45.Search a 2D Matrix.cpp
@@ -1,4 +1,4 @@
-
+https://leetcode.com/problems/search-a-2d-matrix/
 
 //brute force
 
@@ -13,6 +13,40 @@ public:
                 }
             }
         }
+        return false;
+    }
+};
+
+
+binary search
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int start = 0;
+        int end = row*col-1;
+        
+        int mid = start+(end-start)/2;
+        
+        while(start<=end){
+            int element = matrix[mid/col][mid%col]; //important
+            
+            if(element==target){
+                return true;
+            }
+            else if(element<target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+            mid = start+(end-start)/2;
+        }
+        
         return false;
     }
 };
