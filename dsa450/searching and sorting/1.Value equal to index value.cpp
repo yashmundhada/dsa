91946b5ee0c20ac41288b9a18ad82f9b5ceeee32
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-05-27 13:22:33 +0530

    Create 1.Value equal to index value.cpp

---
+++ dsa450/searching and sorting/1.Value equal to index value.cpp
@@ -0,0 +1,20 @@
+https://practice.geeksforgeeks.org/problems/value-equal-to-index-value1330/1#
+
+
+brute force - 
+
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector<int> ans;
+	    
+	    for(int i=0;i<n;i++){
+	        if(arr[i]==i+1){
+	            ans.push_back(arr[i]);
+	        }
+	    }
+	    return ans;
+	}
+};
