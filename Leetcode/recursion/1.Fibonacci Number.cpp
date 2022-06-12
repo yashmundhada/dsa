3a230967e101da228b9fb06ccaaff5202a1a816a
yashmundhada <71841860+yashmundhada@users.noreply.github.com>
2022-06-12 10:54:31 +0530

    Create 1.Fibonacci Number.cpp

---
+++ Leetcode/recursion/1.Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/fibonacci-number/
+
+
+class Solution {
+public:
+    int fib(int n) {
+        //base case
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
+        int ans = fib(n-1) + fib(n-2);
+        
+        return ans;
+        
+    }
+};
